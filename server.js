600c5169e157046a0a54424b9c7223f04b159121
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2022-03-15 08:52:52 -0700

    API working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const axios = require('axios');
 const weather = require('./data/weather.json');
 
 const app = express();
@@ -12,29 +12,42 @@ app.use(cors());
 const PORT = process.env.PORT || 3002;
 
 app.get('/', (request, response) => {
-    response.send('testing testing... is this thing on??')
+  response.send('testing testing...is this thing on??')
 });
 
-app.get('/weather', (request, response) => {
-    let lat = request.query.lat;
-    let lon = request.query.lon;
-    let searchQuery = request.query.searchQuery;
-    const city = weather.find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase());
-    try {
+app.get('/weather', async (request, response) => {
+  let lat = request.query.lat;
+  let lon = request.query.lon;
+  let searchQuery = request.query.searchQuery; // city we searched for
 
-        const weatherArray = city.data.map(day => new Forecast(day));
+  console.log("lat, lon", lat, lon)
 
-        response.status(200).send(weatherArray);
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${lat}&lon=${lon}`;
+  console.log(url);
+  
+  try {
+    const weatherData = await axios.get(url);
+    console.log(weatherData.data.data)
+    const weatherArray = weatherData.data.data.map(day => new Forecast(day));
 
-    } catch (error) {
-        console.log(error);
-        response.status(500).send('city not found')
-    };
+    response.status(200).send(weatherArray);
+
+  } catch(error) {
+    console.log(error);
+    response.status(500).send('city not found')
+  }
+
 })
 
+
 function Forecast(day) {
-    this.day = day.valid_date
-    this.description = day.weather.description
+  this.day = day.valid_date
+  this.description = day.weather.description
 }
 
+
+// Create a function to handle errors from any API call.
+app.use('*', (request, response) => response.status(404).send('that endpoint does not exists'));
+
+// tells the express app which port to listen on
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
